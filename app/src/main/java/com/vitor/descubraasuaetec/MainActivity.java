083f6ec7a7ce1644884ebc17034d140700438a9a
Vitor Oliveira <vituuoliveira1@gmail.com>
2020-10-18 19:57:43 -0300

    Criação da tela de escolha e fontes

--- app/src/main/java/com/vitor/descubraasuaetec/MainActivity.java
+++ app/src/main/java/com/vitor/descubraasuaetec/MainActivity.java
@@ -2,17 +2,37 @@ package com.vitor.descubraasuaetec;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView cursos, unidades;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //escondendo actionbar
-        getSupportActionBar().hide();
 
+        cursos = findViewById(R.id.imgCursos);
+        unidades = findViewById(R.id.imgUnidades);
 
+        cursos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), listaCursos.class);
+                startActivity(intent);
+            }
+        });
+
+        unidades.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), listaUnidades.class);
+                startActivity(intent);
+            }
+        });
     }
 }
